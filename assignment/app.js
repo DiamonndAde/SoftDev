a7119401cdff146ac3271afb5bacf758af3912f0
DiamonndAde <68964863+DiamonndAde@users.noreply.github.com>
2020-08-28 20:57:24 +0100

    Add files via upload

---
+++ assignment/app.js
@@ -0,0 +1,86 @@
+//MAD LIB GAME FIRSTST CHALLENGE
+// let adjective1 = "amazing"
+// let adjective2 = "fun"
+// let adjective3 = "entertainment"
+// let madLib = `The intro to Javascript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content`
+// console.log(madLib)
+
+
+//SECOND CHALLENGE
+// let bill = 10.25 + 3.99 + 7.15
+// let tip = 0.5 * bill
+// let total = bill + tip
+// console.log(`$${total.toFixed(2)}`)
+
+
+// THIRD CHALLENGE
+// let firstName = "Ayomide"
+// let interest = "football"
+// let hobby = "play games"
+// let awesomeMessage = `Hi, my name is ${firstName}. I love ${interest}. In my spare time, I like to ${hobby}`
+// console.log(awesomeMessage)
+
+
+//  FOURTH CHALLENGE
+// let flavour = "chocolate"
+// let vessel = "cone"
+// let toppings = "sprinkles"
+// if((flavour == "chocolate" || flavour == "vanilla") && (vessel == "cone" || vessel == "bowl") && (toppings == "sprinkles" || toppings == "peanuts")){
+//     console.log(`I'd like two scoops of ${flavour} ice cream in a ${vessel} with ${toppings}.`)
+// }
+
+
+// NUMBER 2 FIRST CHALLENGE
+// let shirtWidth = 19.99
+// let shirtLength = 28.99
+// let shirtSleeve = 8.379
+// if((shirtWidth >= 18 && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve < 8.38)){
+//     console.log("S")
+// }else if((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63)){
+//     console.log("M")
+// }else if((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve < 8.88)){
+//     console.log("L")
+// }else if((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63)){
+//     console.log("XL")
+// }else if((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13)){
+//     console.log("2XL")
+// }else if((shirtWidth == 28) && (shirtLength == 34) && (shirtSleeve == 10.13)){
+//     console.log("3XL")
+// }else{
+//     console.log("NA")
+// }
+
+
+// NUMBER 3 FIRST CHALLENGE
+// let eatsPlants = true
+// let eatsAnimals = true
+
+// console.log(eatsPlants && eatsAnimals ? "Omnivore" : eatsPlants ? "Herbivore" : eatsAnimals ? "Carnivore" : undefined)
+
+
+// JULIAJAMES
+// let x = 1
+// while(x <= 20){
+//     if(x % 3 == 0 && x % 5 == 0){
+//         console.log("JuliaJames")
+//     }else if(x % 3 == 0){
+//         console.log("Julia")
+//     }else if(x % 5 ==0){
+//         console.log("James")
+//     }else(console.log(x))
+//     x += 1
+// }
+
+// LOOP CHALLENGE 1
+// for(let x = 9; x >= 1; x--){
+//     console.log("hello " + x)
+// }
+
+
+// FACTORIAL
+x = 5
+solution = 1
+for(let i = 1; i <= x; i++){
+    solution *= i
+}
+console.log(solution)
